ac76acbda8e322d42493ea602d354c0d8cfdd162
Patrick <pdougall1@gmail.com>
2014-10-22 01:12:56 -0400

    improve personal logo
    Orange robot big time!

--- app/components/personal-logo.js
+++ app/components/personal-logo.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	tagName: 'img',
+	attributeBindings: ['src'],
+	src: '/assets/images/orange_robot_2.svg',
+	classNames: ['personal-logo']
 });
