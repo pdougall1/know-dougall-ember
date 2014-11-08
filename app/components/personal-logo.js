579e26a19490594f9ffb9492c9b8bce054ac5455
Patrick <pdougall1@gmail.com>
2014-11-08 18:07:47 -0500

    keep logo on top

--- app/components/personal-logo.js
+++ app/components/personal-logo.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	attributeBindings: ['style'],
+	style: "z-index:10;" +
+	       "position: absolute;" +
+           "top: 20px;" +
+           "left: 20px;"
 });
