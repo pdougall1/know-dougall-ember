21a3c4ab9e43c746a498199675f612c466235c0a
Patrick <pdougall1@gmail.com>
2014-11-16 10:19:08 -0500

    adds right sidebar to edit

--- app/controllers/posts/edit.js
+++ app/controllers/posts/edit.js
@@ -15,5 +15,30 @@ export default Ember.ObjectController.extend({
 				route: 'posts.index'
 			}
 		]
-	}.property()
+	}.property(),
+
+	newAnnotation: function () {
+		var annotation = this.store.createRecord('annotation');
+		annotation.set('post', this.get('content'))
+		return annotation;
+	}.property(),
+
+	resetNewAnnotation: function () {
+		var annotation = this.store.createRecord('annotation');
+		annotation.set('post', this.get('content'))
+		this.set('newAnnotation', annotation);
+	},
+
+	actions: {
+		submitAnnotation: function (annotation) {
+			var _this = this
+			annotation.save().then(function (annotation) {
+				_this.get('annotations').then(function (annotations) {
+					annotations.pushObject(annotation);
+					_this.resetNewAnnotation();
+				});
+			});
+		}
+	}
+
 });
