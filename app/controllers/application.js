da644865eda46dec358512a27862822687166fd7
Patrick <pdougall1@gmail.com>
2014-11-15 21:34:40 -0500

    strip out navigation from applications controller
    this is producing way too many dependancies in the appellation
    controller.  It requires all routes to be passed into the needs
    property :(

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	needs: ['friends-graph/show', 'friends-graph/index', 'posts/index', 'post/show', 'post/new', 'post/edit'],
-
-	getPropertyValue: function (propertyName) {
-		var currentPath = this.get('currentPath').replace(".", "/")
-		return this.get('controllers.' + currentPath +  '.' + propertyName)
-	},
-
-	controllerSpecificProperties: ['navLinks', 'leftSidebarClass'],
-
-	setControllerSpecificProperties: function () {
-		var context = this
-		if (this.get('currentPath') != 'home') {
-			this.get('controllerSpecificProperties').forEach( function (prop) {
-				var val = context.getPropertyValue(prop)
-				context.set(prop, val);
-			});
-		}
-	}.observes('currentPath')
-
 });
