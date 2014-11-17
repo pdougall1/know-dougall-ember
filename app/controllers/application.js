6dbe3a5b8515a841ba6fdeadb1919b01bb2596e0
Patrick <pdougall1@gmail.com>
2014-11-17 01:09:49 -0500

    adds links to home page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	needs: ['resume', 'friends-graph/show', 'friends-graph/index', 'posts/index', 'posts/show', 'posts/new', 'posts/edit', 'application'],
+	needs: ['home', 'friends-graph/show', 'friends-graph/index', 'posts/index', 'posts/show', 'posts/new', 'posts/edit', 'application'],
 
 	getPropertyValue: function (propertyName) {
 		var currentPath = this.get('currentPath').replace(".", "/")
@@ -12,12 +12,12 @@ export default Ember.Controller.extend({
 
 	setControllerSpecificProperties: function () {
 		var context = this
-		if (this.get('currentPath') != 'home') {
+		// if (this.get('currentPath') != 'home') {
 			this.get('controllerSpecificProperties').forEach( function (prop) {
 				var val = context.getPropertyValue(prop)
 				context.set(prop, val);
 			});
-		}
+		// }
 	}.observes('currentPath')
 
 });
