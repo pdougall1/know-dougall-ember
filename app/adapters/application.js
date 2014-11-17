1c15937cb1aa55cd3f24f61bd106f1a6d8e53dee
Patrick <pdougall1@gmail.com>
2014-11-17 01:10:10 -0500

    use local domain variables

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
 	namespace: 'api',
-	host: 'http://localhost:3000'
+	host: KnowDougall.KNOW_DOUGALL_RAILS_DOMAIN
 });
--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-	host: 'http://localhost:5000'
+	host: KnowDougall.FRIENDS_GRAPH_DOMAIN
 });
